f1b934af37fb8251f4cd6232eaf4654d987deec9
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-07-01 21:22:30 -0300

    criou componente de footer ajustando na tela para mobile e desktop

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,102 @@
+import Link from 'next/link'
+
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>
+    <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contact
+        </Heading>
+
+        <a
+          href="mailto:igorcotrim.dev@gmail.com"
+          target="_blank"
+          rel="noopenner, noreferrer"
+        >
+          igorcotrim.dev@gmail.com
+        </a>
+        <a
+          href="https://api.whatsapp.com/send?phone=5577991880997"
+          target="_blank"
+          rel="noopenner, noreferrer"
+        >
+          +55 77 991880997
+        </a>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Follow us
+        </Heading>
+
+        <nav aria-labelledby="social media">
+          <a
+            href="https://www.instagram.com/igorcotrim_/?hl=pt-br"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://twitter.com/igorcotrim__"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UC1lpfwQByrdxOb4UUQS97zg/featured"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.twitch.tv/igorcotrim_"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Twitch
+          </a>
+        </nav>
+      </S.Column>
+
+      <S.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Links
+        </Heading>
+
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Buscar</a>
+          </Link>
+        </nav>
+      </S.Column>
+
+      <S.Column aria-labelledby="footer-contact">
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Location
+        </Heading>
+        <span>Lorem ipsum dolor sit.</span>
+        <span>Lorem Ipsum</span>
+        <span>Lorem, ipsum dolor.</span>
+      </S.Column>
+    </S.Content>
+
+    <S.Copyright>Won Games 2020 © All rights reserved.</S.Copyright>
+  </S.Wrapper>
+)
+
+export default Footer

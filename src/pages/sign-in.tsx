3d459a766f057869babbf02bcc16e810a34760ee
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-07-12 10:40:16 -0300

    criou conexão entre páginas

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,5 +1,11 @@
 import Auth from 'templates/Auth'
 
+import FormSignIn from 'components/FormSignIn'
+
 export default function SignIn() {
-  return <Auth title="Sign In">children</Auth>
+  return (
+    <Auth title="Sign In">
+      <FormSignIn />
+    </Auth>
+  )
 }
--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -49,6 +49,11 @@ export const BannerContent = styled.div`
     grid-template-columns: 1fr;
     justify-content: space-around;
     height: 100%;
+
+    a {
+      width: fit-content;
+      height: fit-content;
+    }
   `}
 `
 

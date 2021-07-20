4e54ad7d3450e7a79f7f600837820c198f6cc758
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-07-19 21:09:07 -0300

    criou componente de GameInfo

---
+++ src/components/GameInfo/mock.ts
@@ -0,0 +1,6 @@
+export default {
+  title: 'Borderlands 3',
+  price: '215,00',
+  description:
+    'Experience the epic space strategy games that redefined the RTS genre. Control ypur fleet and build an armada across more than 30 single-player misssions'
+}

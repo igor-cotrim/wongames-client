fa3732568cc2c7e64018cefcffea5380d3b6275b
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-08-09 20:56:08 -0300

    Criou componente de ExploreSidebar

---
+++ src/components/ExploreSidebar/index.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import { Close, FilterList } from '@styled-icons/material-outlined'
+
+import Button from 'components/Button'
+import Checkbox from 'components/Checkbox'
+import Heading from 'components/Heading'
+import Radio from 'components/Radio'
+
+import * as S from './styles'
+
+type Field = {
+  label: string
+  name: string
+}
+
+type Values = {
+  [field: string]: boolean | string
+}
+
+export type ItemProps = {
+  title: string
+  name: string
+  type: 'checkbox' | 'radio' | string
+  fields: Field[]
+}
+
+export type ExploreSidebarProps = {
+  items: ItemProps[]
+  initialValues?: Values
+  onFilter: (values: Values) => void
+}
+
+const ExploreSidebar = ({
+  items,
+  initialValues = {},
+  onFilter
+}: ExploreSidebarProps) => {
+  const [values, setValues] = useState(initialValues)
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleChange = (name: string, value: string | boolean) => {
+    setValues((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleFilter = () => {
+    onFilter(values)
+    setIsOpen(false)
+  }
+
+  return (
+    <S.Wrapper isOpen={isOpen}>
+      <S.Overlay aria-hidden={isOpen} />
+      <S.IconWrapper>
+        <FilterList aria-label="open filters" onClick={() => setIsOpen(true)} />
+        <Close aria-label="close filters" onClick={() => setIsOpen(false)} />
+      </S.IconWrapper>
+
+      <S.Content>
+        {items.map((item) => (
+          <S.Items key={item.title}>
+            <Heading lineBottom lineColor="secondary" size="small">
+              {item.title}
+            </Heading>
+
+            {item.type === 'checkbox' &&
+              item.fields.map((field) => (
+                <Checkbox
+                  key={field.name}
+                  name={field.name}
+                  label={field.label}
+                  labelFor={field.name}
+                  isChecked={!!values[field.name]}
+                  onCheck={(v) => handleChange(field.name, v)}
+                />
+              ))}
+
+            {item.type === 'radio' &&
+              item.fields.map((field) => (
+                <Radio
+                  key={field.name}
+                  id={field.name}
+                  name={item.name}
+                  value={field.name}
+                  label={field.label}
+                  labelFor={field.name}
+                  defaultChecked={field.name === values[item.name]}
+                  onChange={() => handleChange(item.name, field.name)}
+                />
+              ))}
+          </S.Items>
+        ))}
+
+        <S.Footer>
+          <Button fullWidth size="medium" onClick={handleFilter}>
+            Filter
+          </Button>
+        </S.Footer>
+      </S.Content>
+    </S.Wrapper>
+  )
+}
+
+export default ExploreSidebar

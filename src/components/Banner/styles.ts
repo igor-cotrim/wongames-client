7dff14c0a1917ce237283298d338d632371e5141
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-08-12 10:31:14 -0300

    Criou componente UserDropdown

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -17,7 +17,7 @@ export const Wrapper = styled.main`
   `}
 
   ${media.greaterThan('medium')`
-    box-shadow: 0 0.4rem 0.5rem 0 rgba(0,0,0,0.2)
+    box-shadow: 0 0.4rem 0.5rem 0 rgba(0,0,0,0.2);
   `}
 `
 

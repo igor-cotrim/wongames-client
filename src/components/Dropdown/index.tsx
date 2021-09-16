071c580b0b92a6713b3a18e7c2df491d5a97470c
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-09-16 19:15:35 -0300

    criou overlay do dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -14,6 +14,7 @@ const Dropdown = ({ children, title }: DropdownProps) => {
       <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
 
       <S.Content aria-hidden={!isOpen}>{children}</S.Content>
+      <S.Overlay aria-hidden={!isOpen} onClick={() => setIsOpen(!isOpen)} />
     </S.Wrapper>
   )
 }

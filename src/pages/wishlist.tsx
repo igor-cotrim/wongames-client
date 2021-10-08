91a2895a49f4b40c58bdd15653587b4df98392da
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-10-07 21:36:21 -0300

    criou página de wishlist utilizando o Apollo Cache

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,24 +1,35 @@
-import { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { GetServerSidePropsContext } from 'next'
 
 import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
 import gamesMock from 'components/GameCardSlider/mock'
+import { initializeApollo } from 'utils/apollo'
+import { gamesMapper, highlightMapper } from 'utils/mappers'
+import protectedRoutes from 'utils/protected-routes'
 
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
-
-import { gamesMapper, highlightMapper } from 'utils/mappers'
-import { initializeApollo } from 'utils/apollo'
-import protectedRoutes from 'utils/protected-routes'
+import {
+  QueryWishlist,
+  QueryWishlistVariables
+} from 'graphql/generated/QueryWishlist'
+import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
 }
 
-export const getServerSideProps: GetServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
-  const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo(null, session)
+
+  if (!session) return {}
+
+  await apolloClient.query<QueryWishlist, QueryWishlistVariables>({
+    query: QUERY_WISHLIST,
+    variables: {
+      identifier: session.user?.email as string
+    }
+  })
 
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
@@ -27,6 +38,7 @@ export const getServerSideProps: GetServerSideProps = async (
   return {
     props: {
       session,
+      initialApolloState: apolloClient.cache.extract(),
       games: gamesMock,
       recommendedTitle: data.recommended?.section?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -8,49 +8,60 @@ import { Divider } from 'components/Divider'
 import { HighlightProps } from 'components/Highlight'
 import GameCard, { GameCardProps } from 'components/GameCard'
 import Empty from 'components/Empty'
+import Loader from 'components/Loader'
+
+import { useWishlist } from 'hooks/use-wishlist'
+
+import * as S from './styles'
 
 export type WishlistTemplateProps = {
-  games?: GameCardProps[]
   recommendedTitle?: string
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
 }
 
 const Wishlist = ({
-  games = [],
   recommendedTitle,
   recommendedGames,
   recommendedHighlight
-}: WishlistTemplateProps) => (
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        Wishlist
-      </Heading>
+}: WishlistTemplateProps) => {
+  const { items, loading } = useWishlist()
 
-      {games.length >= 1 ? (
-        <Grid>
-          {games?.map((game, index) => (
-            <GameCard key={`wishlist-${index}`} {...game} />
-          ))}
-        </Grid>
-      ) : (
-        <Empty
-          title="Your wishlist is empty"
-          description="Games added to your wishlist will appear here"
-          hasLink
-        />
-      )}
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          Wishlist
+        </Heading>
 
-      <Divider />
-    </Container>
+        {loading ? (
+          <S.Loading>
+            <Loader />
+          </S.Loading>
+        ) : items.length >= 1 ? (
+          <Grid>
+            {items?.map((game, index) => (
+              <GameCard key={`wishlist-${index}`} {...game} />
+            ))}
+          </Grid>
+        ) : (
+          <Empty
+            title="Your wishlist is empty"
+            description="Games added to your wishlist will appear here"
+            hasLink
+          />
+        )}
 
-    <Showcase
-      title={recommendedTitle || 'You may like these games'}
-      games={recommendedGames}
-      highlight={recommendedHighlight}
-    />
-  </Base>
-)
+        <Divider />
+      </Container>
+
+      <Showcase
+        title={recommendedTitle || 'You may like these games'}
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
+    </Base>
+  )
+}
 
 export default Wishlist

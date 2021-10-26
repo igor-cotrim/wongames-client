161d3056b39855da865355014f48d0ad30359588
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-10-26 20:27:51 -0300

    evitou erros no next no session em páginas autenticadas

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -18,6 +18,10 @@ export const getServerSideProps: GetServerSideProps = async (
   const session = await protectedRoutes(context)
   const apolloClient = initializeApollo(null, session)
 
+  if (!session) {
+    return { props: {} }
+  }
+
   const { data } = await apolloClient.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
   })

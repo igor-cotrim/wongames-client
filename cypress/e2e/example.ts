05de79d6a1ee45da1d6f63e1e5d5f6c783cdbd0d
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-11-03 15:43:57 -0300

    começou a fazer os testes e2e começando da Home Page

--- cypress/e2e/example.ts
+++ cypress/e2e/example.ts
@@ -1,7 +1,7 @@
 /// <reference path="../support/index.d.ts" />
 
-describe('Cypress TS', () => {
-  it.skip('should go to go Google', () => {
+describe.skip('Cypress TS', () => {
+  it('should go to go Google', () => {
     cy.google()
   })
 
